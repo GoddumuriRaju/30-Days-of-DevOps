af43de29e02d00340f19af2c47da4c7fe247f184
Raju <rajugoddumuri@gmail.com>
2024-12-29 17:25:36 +0530

    latest commit (Day-08,09)

---
+++ Day-10(Terraform)/variables.tf
@@ -0,0 +1,36 @@
+variable "access_key" {
+        description = "Access key to AWS console"
+}
+variable "secret_key" {
+        description = "Secret key to AWS console"
+}
+
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "DEMO-instance"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+        default = "subnet-06b925bb26cf7db49"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+        default = "ami-005fc0f236362e99f"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        default = "master"
+}
